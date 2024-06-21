3dde13f91f797116cbc528bb2f54bb3c302ec6a0
นภสร ทองโสภา <npsbell@Macintosh-4.local>
2024-06-21 15:49:07 +0700

    submit: React Component Styling Assignment

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,47 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import FrownIcon from "../assets/Frownicon.jsx";
+import CheckIcon from "../assets/Frownicon.jsx";
+import TriangleIcon from "../assets/Frownicon.jsx";
+import CircleIcon from "../assets/Frownicon.jsx";;
 
-function Alert() {}
+function Alert(props) {
+    let bgColor = undefined;
+    let AlertIcon = undefined;
+
+  if (props.icon === "error") {
+    bgColor = "red";
+    AlertIcon = FrownIcon
+  } else if (props.icon === "warning") {
+    bgColor = "orange";
+    AlertIcon = TriangleIcon
+  } else if (props.icon === "info") {
+    bgColor = "yellow";
+    AlertIcon = CircleIcon
+  } else if (props.icom === "success") {
+    bgColor = "green"
+    AlertIcon = CheckIcon
+  }
+
+    return(
+        <div
+        css={css`
+        display: flex
+        flex-direction: colume
+        background-color: ${bgColor}
+        align-items: center
+        justify-content: start
+        margin: 10px
+        width: 600px
+        height: 70px
+        font-size: 16px
+        margin: 10px
+        `}
+        >
+            {props.text}
+        </div>
+    )
+}
+
+export default Alert
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,30 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  let bgColor = undefined
+  if (props.color == "primary") {
+    bgColor = "blue";
+  } else if (props.color =="secondary") {
+    bgColor = "lightblue";
+  }
+    return(
+        <button
+        css={css`
+        background-color: ${bgColor}
+        margin: 10px
+        width: 150px
+        height: 50px
+        border: none
+        border-radius: 5px
+        color: white
+        font-size: 16px
+        `}
+        >
+            Button
+        </button>
+    )
+}
+
+export default Button;
